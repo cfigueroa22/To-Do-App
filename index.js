08c7889822be2b1ceb2a15274f85d43e1a10d13e
Carlos Figueroa <carfig7186@students.ecpi.edu>
2023-04-23 19:25:53 -0400

    delete button added to tasks added

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const taskInput = document.getElementById("task-input");
 const enterTask = document.querySelector(".enter-task");
 const tasks = document.querySelector(".list");
+const deleteButton = document.querySelector(".delete");
 
 enterTask.addEventListener("click", function () {
   const newTask = taskInput.value;
   if (newTask) {
     const li = document.createElement("li");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("delete");
     li.textContent = newTask;
+    deleteBtn.textContent = `X`;
     tasks.appendChild(li);
+    tasks.appendChild(deleteBtn);
     taskInput.value = "";
   }
 });
